f1fd7676cf910bab1eb9a197e60f86b5de4b39b5
Nadzeya Silchankava <nadzeya@Nadzeyas-MacBook-Air.local>
2025-03-16 18:21:17 +0100

    poprawa blędów + iteracje bez 0

--- zadanie1/bisekcja.py
+++ zadanie1/bisekcja.py
@@ -5,38 +5,31 @@ def bisekcja_funkcji(a,b,funkcja,x_srodek):
     x_srodek, a, b=sprawdz_warunki(funkcja,x_srodek,a,b)
     return x_srodek,a,b
 
-#realizacja bisekcji dla wielomianu
-def bisekcja_wielomian(a,b,wspolczynniki,x_srodek):
-    horner_x_srodek = horner(x_srodek, wspolczynniki)
-    if horner_x_srodek == 0:
-        return x_srodek,a,b
-    if horner_x_srodek * horner(b, wspolczynniki) < 0:
-        a = x_srodek
-    if horner_x_srodek * horner(a, wspolczynniki) < 0:
-        b = x_srodek
-    return x_srodek,a,b
-
 #ogólna realizacja bisekcji dla wszystkich wbudowanych funkcji
 def bisekcja(a, b, wspolczynniki, epsilon,iteracje, kryterium, czy_wielomian,funkcja):
     ile_iteracji = 0
     if czy_wielomian:
         #dokładność
         if kryterium == 'a':
-            x_srodek = 0
+            x_srodek = (a + b) / 2
+            if abs(horner(x_srodek, wspolczynniki)) < epsilon:
+                return x_srodek, 1
             while abs(horner((a + b) / 2, wspolczynniki)) >= epsilon:
-                x_srodek,a,b=bisekcja_wielomian(a, b, wspolczynniki, (a + b)/2)
+                x_srodek,a,b=sprawdz_warunki_wielomianu(a, b, wspolczynniki, (a + b)/2)
                 ile_iteracji += 1
             return x_srodek, ile_iteracji
         # iteracje
         else:
             x_srodek = 0
             for i in range(iteracje):
-                x_srodek,a,b = bisekcja_wielomian(a, b, wspolczynniki, (a + b)/2)
+                x_srodek,a,b = sprawdz_warunki_wielomianu(a, b, wspolczynniki, (a + b)/2)
             return x_srodek, iteracje
     else:
         #dokładność
         if kryterium == 'a':
-            x_srodek = 0
+            x_srodek = (a + b) / 2
+            if abs(funkcja(x_srodek)) < epsilon:
+                return x_srodek, 1
             while abs(funkcja(x_srodek)) >= epsilon:
                 x_srodek,a,b=bisekcja_funkcji(a,b,funkcja,(a + b)/2)
                 ile_iteracji += 1
--- zadanie1/falsi.py
+++ zadanie1/falsi.py
@@ -13,13 +13,7 @@ def falsi_wielomianu(a,b,wspolczynniki,horner_a,horner_b):
     if horner_a == horner_b:
         return a,b,(a + b) / 2
     x0 = a - (horner_a / (horner_b - horner_a)) * (b - a)
-    horner_x0 = horner(x0, wspolczynniki)
-    if horner_x0 == 0:
-        return a,b,x0
-    if horner_x0 * horner_b < 0:
-        a = x0
-    if horner_x0 * horner_a < 0:
-        b = x0
+    x0, a, b = sprawdz_warunki_wielomianu(a,b,wspolczynniki,x0)
     return a,b,x0
 
 #ogólna realizacja regula falsi dla wszystkich wbudowanych funkcji
@@ -32,9 +26,11 @@ def falsi(a, b, wspolczynniki, epsilon,iteracje, kryterium, czy_wielomian,funkcj
         if kryterium == 'a':
             x0 = a - (horner_a / (horner_b - horner_a)) * (b - a)
             horner_x0 = horner(x0, wspolczynniki)
+            if abs(horner_x0) < epsilon:
+                return x0, 1
             while abs(horner_x0) >= epsilon:
                 a,b,x0=falsi_wielomianu(a,b,wspolczynniki,horner(a, wspolczynniki),horner(b, wspolczynniki))
-                # horner_x0 = horner(x0, wspolczynniki)
+                horner_x0 = horner(x0, wspolczynniki)
                 ile_iteracji += 1
             return x0, ile_iteracji
         # iteracje
@@ -47,6 +43,8 @@ def falsi(a, b, wspolczynniki, epsilon,iteracje, kryterium, czy_wielomian,funkcj
         #dokładność
         if kryterium == 'a':
             x0 = a - (funkcja(a) / (funkcja(b) - funkcja(a))) * (b - a)
+            if abs(funkcja(x0)) < epsilon:
+                return x0, 1
             while abs(funkcja(x0)) >= epsilon:
                 a,b,x0=falsi_funkcji(a,b,funkcja)
                 ile_iteracji += 1
